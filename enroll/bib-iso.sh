b31af236ec8da97bf8c4f0f9a4113f25534c6c63
Fernando Lozano <flozano@redhat.com>
2025-12-11 21:01:43 +0000

    initial bootc images for enrollment

---
+++ enroll/bib-iso.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo podman run --rm -it --privileged --pull=newer \
+    --security-opt label=type:unconfined_t \
+    -v /var/lib/containers/storage:/var/lib/containers/storage \
+    -v $(pwd)/config.toml:/config.toml \
+    -v $(pwd)/output:/output \
+    registry.redhat.io/rhel9/bootc-image-builder:9.6 \
+    --type iso --config /config.toml \
+    --progress=verbose \
+    registry.lab.example.com:5000/rhem-enroll:v1.0
+
---
+++ enroll/virt-install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ ! "${1}" =~ ^[0-9]$ ]]; then
+    echo 'Please provide a single-digit number [0-9] to identify your test VM' 1>&2
+    exit 1
+fi
+
+echo "Creating VM edge-${1}, use port 8${1}22 for SSH."
+
+LABEL=RHEL-9-6-BaseOS-x86_64
+
+virt-install --name edge-{$1} \
+--vcpus 2 --memory 4096 \
+--disk size=20 \
+--network passt,portForward0=8${1}22:22,portForward1=8${1}08:80 \
+--location httpd-enroll.iso,kernel=/images/pxeboot/vmlinuz,initrd=/images/pxeboot/initrd.img \
+--os-variant rhel9.0 \
+--graphics=none \
+--extra-arg inst.ks=hd:LABEL=$LABEL:/osbuild.ks \
+--extra-arg console=ttyS0 -v
