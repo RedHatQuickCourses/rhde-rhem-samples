1b657050b63bb6d3ce9a3e845f54bf87261c0623
Fernando Lozano <flozano@redhat.com>
2026-03-12 16:21:25 -0300

    indent at two spaces

--- fleet/count-devices-multipleowners.sh
+++ fleet/count-devices-multipleowners.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 flightctl get devices -o json | jq '[.items[]
-    | .status.conditions[]?
-    | select(.type == "MultipleOwners" and .status == "True")]
-    | length'
+  | .status.conditions[]?
+  | select(.type == "MultipleOwners" and .status == "True")]
+  | length'
--- fleet/get-devices-multipleowners.sh
+++ fleet/get-devices-multipleowners.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 flightctl get devices -o json | jq -r '.items[]
-    | select(.status.conditions[]? | .type == "MultipleOwners" and .status == "True")
-    | "Device: \(.metadata.name)\nLabels: \(.metadata.labels // "None")\n---"'
+  | select(.status.conditions[]? | .type == "MultipleOwners" and .status == "True")
+  | "Device: \(.metadata.name)\nLabels: \(.metadata.labels // "None")\n---"'
